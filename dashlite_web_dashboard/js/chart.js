0bd8e97cc1fea06c4086119c041226eef30eee6e
{subinjang} <{tnqlslove56@gmail.com}>
2023-08-31 17:23:39 +0900

    .

--- dashlite_web_dashboard/js/chart.js
+++ dashlite_web_dashboard/js/chart.js
@@ -301,3 +301,80 @@ $(document).ready(function(){
         }
     });
 });
+
+// chart.js - type: bar
+$(document).ready(function(){
+    const ctx = document.getElementById('analyticAuData');
+
+    new Chart(ctx, {
+        type: 'bar', //pie, line, doughnut, polarArea
+        data: {
+            labels: ["01 Jan", "02 Jan", "03 Jan", "04 Jan", "05 Jan", "06 Jan", "07 Jan", "08 Jan", "09 Jan", "10 Jan", "11 Jan", "12 Jan", "13 Jan", "14 Jan", "15 Jan", "16 Jan", "17 Jan", "18 Jan", "19 Jan", "20 Jan", "21 Jan", "22 Jan", "23 Jan", "24 Jan", "25 Jan", "26 Jan", "27 Jan", "28 Jan", "29 Jan", "30 Jan"],
+            dataUnit: "People",
+            datasets: [{
+                label: '',
+                color: "#9cabff",
+                data: [1110, 1220, 1310, 980, 900, 770, 1060, 830, 690, 730, 790, 950, 1100, 800, 1250, 850, 950, 450, 900, 1e3, 1200, 1250, 900, 950, 1300, 1200, 1250, 650, 950, 750],
+                backgroundColor: "#9cabff",
+                borderWidth: 2,
+                borderColor: "#9cabff",
+                // borderSkipped: "bottom",
+                // barThickness: 15
+            }]
+        },
+        options: {
+            maintainAspectRatio :false,
+            legend:{
+                labels: {
+                    display:false,
+                    fontColor: 'rgb(255, 255, 255)'
+                },
+            },
+            plugins:{
+                legend: {
+                    display: false
+                },
+                datalabels: {
+                    display: false
+                },
+                tooltip:{
+                    enabled: true,
+                    usePointStyle: true,
+                    displayColors: false,
+                    titleSpacing: 3,
+                    backgroundColor: "#eff6ff",
+                    titleFont: {
+                        size: 11
+                    },
+                    titleColor:"#9eaecf",
+                    titleMarginBottom: 4,
+                    titleAlign : "left",
+                    bodyFont: {
+                        size: 10
+                    },
+                    bodyColor:"#9eaecf",
+                    bodySpacing: 3,
+                    padding: 8
+                }
+            },
+            scales: {
+                x : {
+                    display : 0,
+                    // grid : {
+                    //     display: false, 
+                    //     drawBorder: false
+                    // }, 
+                    gridLines: {
+                        display: 1
+                    }
+                },
+                y : {
+                    display : 1,
+                    gridLines: {
+                        display: 1
+                    }
+                }
+            }
+        }
+    });
+});
